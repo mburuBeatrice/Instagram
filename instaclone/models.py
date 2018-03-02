16369f02cfb63e59047438bc34947841cc66a39d
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-03-02 12:58:22 +0300

    add profile model

--- instaclone/models.py
+++ instaclone/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    photo = models.CharField(max_length =30)
+    photo = models.ImageField(upload_to = 'image/')
     name = models.CharField(max_length =30)
     caption = models.CharField(max_length =250)
     comment = models.CharField(max_length =100)
 
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['name']
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'profile/')
+    bio = models.CharField(max_length =250)
 
