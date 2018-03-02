d26fde3a732707f9d2a0ae5063cf827fb9f857bd
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-03-02 12:25:48 +0300

    adds an image model

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    photo = models.CharField(max_length =30)
+    name = models.CharField(max_length =30)
+    caption = models.CharField(max_length =250)
+    comment = models.CharField(max_length =100)
+
+
