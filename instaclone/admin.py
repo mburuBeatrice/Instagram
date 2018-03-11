1e972d9e04e79aa273a74dc9acc5447b0dd0572d
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-03-11 11:36:53 +0300

    customized admin page

--- instaclone/admin.py
+++ instaclone/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Image,Profile
+from .models import Post,Profile
 
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["name", "lastupdated","timestamp"]
+    list_display_links = ["lastupdated"]
+    list_editable = ["name"]
+    list_filter = ["lastupdated","timestamp"]
+    search_fields = ["name","caption"]
+    class Meta:
+        model = Post
 # Register your models here.
-admin.site.register(Image)
+admin.site.register(Post,PostModelAdmin)
 admin.site.register(Profile)
+
