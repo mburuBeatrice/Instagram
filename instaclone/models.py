cdb5d28a3287480b4eca8a619fc6c0a0a3a3de05
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-03-11 11:38:53 +0300

    changed image model to post and added timestamps

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,45 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
+from django.core.urlresolvers import reverse
 # Create your models here.
 
 
 class Profile(models.Model):
-    profile_photo = models.ImageField(upload_to = 'profile/')
+    profile_photo = models.ImageField(upload_to = 'profile/', null=True,blank=True)
     bio = models.CharField(max_length =250)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE, default=1)
     def __str__(self):
         return self.bio
     class Meta:
         ordering = ['bio']
-    
-    def save_profile(self):
-        self.save()
-    def delete_profile(self):
-        self.delete()
-    def update_profile(self):
-        self.update()
-    def find_profile(name):
-        pass   
+    def get_absolute_url(self):
+        return reverse("instaclone:profile_detail", kwargs={"id": self.id})
 
-class Image(models.Model):
-    photo = models.ImageField(upload_to = 'image/')
+    # def profile(self):
+    #     id = self.kwargs.get("id")
+    #     return Profile.objects.get(id=id)
+   
+class Post(models.Model):
+    photo = models.ImageField(upload_to = 'images/', null=True,blank=True)
     name = models.CharField(max_length =30)
-    caption = models.CharField(max_length =250)
-    comment = models.CharField(max_length =100)
-    profile = models.ForeignKey(Profile)
+    caption = models.TextField(max_length =250)
+    comment = models.TextField(max_length =100,null=True,blank=True)
+    profile = models.ForeignKey(User,default=1)
     likes = models.CharField(max_length =30, null=True, blank=True)
+    lastupdated = models.DateTimeField(auto_now=True,auto_now_add=False,null=True,blank=True)
+    timestamp = models.DateTimeField(auto_now=False,auto_now_add=True,null=True,blank=True)
 
 
     def __str__(self):
         return self.name
     class Meta:
-        ordering = ['name']
-    def save_image(self):
-        self.save()
-    def delete_image(self):
-        self.delete()
-    def update_caption(self):
-        self.update()
-    def get_image_by_id(self):
-        pass        
+        ordering = ['-timestamp','-lastupdated']
+        
+    def get_absolute_url(self):
+        return reverse("instaclone:post_detail", kwargs={"id": self.id})
